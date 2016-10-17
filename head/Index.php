34acd77a507187fb4ada9c02086b8085c21c6459
Zhang Xingzhi <zhangxingz@servyou.com.cn>
2016-10-17 13:25:02 +0800

    初始化

---
+++ head/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangxingz
+ * Date: 2016/10/17
+ * Time: 12:48
+ */
+
+namespace head\render;
+
+class Index
+{
+    public function index() {
+        echo 1;
+    }
+
+}
